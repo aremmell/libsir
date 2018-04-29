6f39233b9d27167ccd4401ea3525ee655b43d97f
Ryan Lederman <ryan@getsparkage.com>
2018-04-28 21:40:26 -0600

    win32 mutex.

--- sirmutex.c
+++ sirmutex.c
@@ -91,24 +91,87 @@ bool _sirmutex_destroy(sirmutex_t* mutex) {
 #else /* Win32 mutex implementation */
 
 bool _sirmutex_create(sirmutex_t* mutex) {
+
+    assert(mutex);
+
+    if (mutex) {
+        sirmutex_t tmp = CreateMutex(NULL, FALSE, NULL);
+
+        if (!tmp) {
+            _sir_handleerr(GetLastError());
+            return false;
+        }
+
+        *mutex = tmp;
+        return true;
+    }
+
     return false;
 }
 
 bool _sirmutex_trylock(sirmutex_t* mutex) {
-    return false;
+    return _sirmutex_waitwin32(mutex, 0);
 }
 
 bool _sirmutex_lock(sirmutex_t* mutex) {
-    return false;
+    return _sirmutex_waitwin32(mutex, INFINITE);
 }
 
 bool _sirmutex_unlock(sirmutex_t* mutex) {
+
+    assert(mutex);
+
+    if (mutex) {
+        BOOL release = ReleaseMutex(*mutex);
+
+        if (!release) 
+            _sir_handleerr(GetLastError());
+
+        return FALSE != release;
+    }
+
     return false;
 }
 
 bool _sirmutex_destroy(sirmutex_t* mutex) {
+
+    assert(mutex);
+
+    if (mutex) {
+        BOOL close = CloseHandle(*mutex);
+
+        if (!close) 
+            _sir_handleerr(GetLastError());
+
+        return FALSE != close;
+    }
+
     return false;
 }
+
+static inline bool _sirmutex_waitwin32(sirmutex_t* mutex, DWORD msec) {
+
+    assert(mutex);
+
+    if (mutex) {
+        DWORD wait = WaitForSingleObject(*mutex, msec);
+
+        switch(wait) {
+            case WAIT_ABANDONED:
+            case WAIT_FAILED:
+                _sir_selflog("%s: warning: WaitForSingleObject returned 0x%08lx. danger ahead\n",
+                    __func__, wait);
+                return WAIT_FAILED != wait;
+            break;
+            case WAIT_TIMEOUT:
+            case WAIT_OBJECT_0:
+                return true;
+        }
+    }
+
+    return false;
+}
+
 #endif /* !_WIN32 */
 
 /*! \endcond */
