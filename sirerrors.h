f422800c338f86a3d4f53f00c3223facd9d6fa2b
Ryan M. Lederman <ryan@getsparkage.com>
2018-05-08 13:28:17 -0600

    formatting

--- sirerrors.h
+++ sirerrors.h
@@ -8,30 +8,37 @@
 #include "sirhelpers.h"
 #include "sirtypes.h"
 
-#define SIR_E_NOERROR 0x00000000        /**< The operation completed successfully */
-#define SIR_E_UNKNOWN 0x8fff0000        /**< Represents the unknown error case. */
-#define SIR_E_NOTREADY _sir_mkerror(1)  /**< SIR has not been initialized */
-#define SIR_E_ALREADY _sir_mkerror(2)   /**< SIR is already initialized */
-#define SIR_E_DUPFILE _sir_mkerror(3)   /**< File already managed by SIR */
-#define SIR_E_NOFILE _sir_mkerror(4)    /**< File not managed by SIR */
-#define SIR_E_FCFULL _sir_mkerror(5)    /**< Maximum number of files already managed */
-#define SIR_E_OPTIONS _sir_mkerror(6)   /**< Option flags are invalid */
-#define SIR_E_LEVELS _sir_mkerror(7)    /**< Level flags are invalid */
-#define SIR_E_TEXTSTYLE _sir_mkerror(8) /**< Text style is invalid */
-#define SIR_E_STRING _sir_mkerror(9)    /**< Invalid string argument */
-#define SIR_E_NODEST _sir_mkerror(10)   /**< No destinations registered for level */
-#define SIR_E_PLATFORM _sir_mkerror(11) /**< Platform error code %d: %s */
+#define SIR_E_NOERROR   0x00000000       /**< The operation completed successfully */
+#define SIR_E_UNKNOWN   0x8fff0000       /**< Represents the unknown error case. */
+#define SIR_E_NOTREADY  _sir_mkerror( 1) /**< SIR has not been initialized */
+#define SIR_E_ALREADY   _sir_mkerror( 2) /**< SIR is already initialized */
+#define SIR_E_DUPFILE   _sir_mkerror( 3) /**< File already managed by SIR */
+#define SIR_E_NOFILE    _sir_mkerror( 4) /**< File not managed by SIR */
+#define SIR_E_FCFULL    _sir_mkerror( 5) /**< Maximum number of files already managed */
+#define SIR_E_OPTIONS   _sir_mkerror( 6) /**< Option flags are invalid */
+#define SIR_E_LEVELS    _sir_mkerror( 7) /**< Level flags are invalid */
+#define SIR_E_TEXTSTYLE _sir_mkerror( 8) /**< Text style is invalid */
+#define SIR_E_STRING    _sir_mkerror( 9) /**< Invalid string argument */
+#define SIR_E_NODEST    _sir_mkerror(10) /**< No destinations registered for level */
+#define SIR_E_PLATFORM  _sir_mkerror(11) /**< Platform error code %d: %s */
 
 static const struct {
     sirerror_t       e;
     const sirchar_t* msg;
-} sir_errors[] = {{SIR_E_NOERROR, "The operation completed successfully"},
-    {SIR_E_NOTREADY, "SIR has not been initialized"}, {SIR_E_ALREADY, "SIR is already initialized"},
-    {SIR_E_DUPFILE, "File already managed by SIR"}, {SIR_E_NOFILE, "File not managed by SIR"},
-    {SIR_E_FCFULL, "Maximum number of files already managed"}, {SIR_E_OPTIONS, "Option flags are invalid"},
-    {SIR_E_LEVELS, "Level flags are invalid"}, {SIR_E_TEXTSTYLE, "Text style is invalid"},
-    {SIR_E_STRING, "Invalid string argument"}, {SIR_E_NODEST, "No destinations registered for level"},
-    {SIR_E_PLATFORM, "Platform error code %d: %s"}};
+} sir_errors[] = {
+    { SIR_E_NOERROR, "The operation completed successfully" },
+    { SIR_E_NOTREADY, "SIR has not been initialized" },
+    { SIR_E_ALREADY, "SIR is already initialized" },
+    { SIR_E_DUPFILE, "File already managed by SIR" },
+    { SIR_E_NOFILE, "File not managed by SIR" },
+    { SIR_E_FCFULL, "Maximum number of files already managed" },
+    { SIR_E_OPTIONS, "Option flags are invalid" },
+    { SIR_E_LEVELS, "Level flags are invalid" },
+    { SIR_E_TEXTSTYLE, "Text style is invalid" },
+    { SIR_E_STRING, "Invalid string argument" },
+    { SIR_E_NODEST, "No destinations registered for level" },
+    { SIR_E_PLATFORM, "Platform error code %d: %s" },
+};
 
 void __sir_seterror(sirerror_t err, const sirchar_t* func, const sirchar_t* file, uint32_t line);
 #define _sir_seterror(err) __sir_seterror(err, __func__, __FILE__, __LINE__)
