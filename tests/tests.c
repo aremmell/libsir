188d915ef923f56080d3a5e9724749db1bf63504
Ryan Lederman <ryan@getsparkage.com>
2018-05-10 20:45:13 -0600

    ensure an error message is returned for each possibility

--- tests/tests.c
+++ tests/tests.c
@@ -414,8 +414,9 @@ bool sirtest_errorsanity(void) {
     sirchar_t message[SIR_MAXERROR] = {0};
     for (size_t n = 0; n < (sizeof(errors) / sizeof(errors[0])); n++) {
         _sir_seterror(_sir_mkerror(errors[n].code));
+        memset(message, 0, SIR_MAXERROR);
         uint16_t err = err = sir_geterror(message);
-        pass &= errors[n].code == err;
+        pass &= errors[n].code == err && *message != '\0';
         printf("\t%s = %s\n", errors[n].name, message);
     }
 
