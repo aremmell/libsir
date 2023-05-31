1cf34f705ab9c4c89bb4390cd8a8d8591bd765d9
Ryan M. Lederman <lederman@gmail.com>
2023-05-31 05:19:36 -0600

    small modificaitons to example; need to rework it

--- example/example.c
+++ example/example.c
@@ -29,48 +29,44 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 #include <sir.h>
 #include <sirhelpers.h>
 
-/*
- * This is a basic example of initializing and setting up libsir for use.
- * 
- * NOTE: You can build this CLI app yourself using `make example` or the
- * Visual Studio [Code] project files.
- * 
- * When the program is finished running, you can see the output in the console, and
- * examine the contents of 'libsir-example.log' in the current directory.
- */
 
 int report_error(void);
 
-int main(int argc, char** argv) {
-
-    _SIR_UNUSED(argc);
-    _SIR_UNUSED(argv);
+/**
+ * @brief This is a basic example of initializing and setting up libsir for use.
+ * 
+ * @note You can build this CLI app yourself using `make example` or the
+ * Visual Studio [Code] project files.
+ * 
+ * When the program is finished running, you can see the output in the console,
+ * and examine the contents of 'libsir-example.log' in the current directory.
+ */
+int main(void) {
 
     /*
      * Instantiate the initialization structure.
      * 
-     * NOTE: It is not necessary to retain this structure in memory;
-     * libsir makes a copy of it before returning from sir_init.
+     * @note It is not necessary to retain this structure in memory;
+     * libsir makes a copy of it before returning from ::sir_init.
      */
     sirinit si = {0};
 
     /* Configure levels for stdout. Send debug, information, warning, and notice messages there. */
     si.d_stdout.levels = SIRL_DEBUG | SIRL_INFO | SIRL_WARN | SIRL_NOTICE;
 
-    /* Configure options for stdout. Don't show the time stamp, process ID, or thread ID. */
-    si.d_stdout.opts = SIRO_NOTIME | SIRO_NOPID | SIRO_NOTID;
+    /* Configure options for stdout. Don't show the time stamp or thread ID. */
+    si.d_stdout.opts = SIRO_NOTIME | SIRO_NOPID;
 
     /* Configure levels for stderr. Send error and above there. */
     si.d_stderr.levels = SIRL_ERROR | SIRL_CRIT | SIRL_ALERT | SIRL_EMERG;
 
-    /* Configure options for stderr. Don't show the process ID or thread ID. */
-    si.d_stderr.opts = SIRO_NOPID | SIRO_NOTID;
+    /* Configure options for stderr: don't show the time stamp or thread ID. */
+    si.d_stderr.opts = SIRO_NOTIME | SIRO_NOPID;
 
-    /* Configure options for syslog. Don't send any output there. */
+    /* Configure options for the system logger: don't send any output there. */
     si.d_syslog.levels = SIRL_NONE;
 
     /* Configure a name to associate with our output. */
@@ -87,16 +83,14 @@ int main(int argc, char** argv) {
     if (NULL == fileid1)
         return report_error();
 
-
-    /* Now we're ready to start generating output. */
+    /* Ready to start logging. */
     int n = 12345;
     const char* somestr = "some string that's useful";
     float f = 0.09f;
 
     /*
      * This will be sent to all destinations registered for ::SIRL_DEBUG.
-     * Notice that it is not necessary to add a newline at the end. libsir does
-     * this automatically.
+     * Notice that it is not necessary to include a newline..
      */ 
     sir_debug("some debug data: {n=%d, somestr='%s', f=%.04f}", n, somestr, f);
 
