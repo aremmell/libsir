ed6ff2f96d6ec20a078540b4e42a91372f88cbdd
Ryan M. Lederman <lederman@gmail.com>
2023-07-05 21:28:52 -0600

    use _fstat to validate file descriptors, but don't install invalid parameter handler if SIR_MSVCRT_MINGW is defined.

--- sirhelpers.c
+++ sirhelpers.c
@@ -54,19 +54,19 @@ bool _sir_validfd(int fd) {
     }
 #if !defined(__WIN__)
     int ret = fcntl(fd, F_GETFL);
+#else /* __WIN__ */
+# if !defined(SIR_MSVCRT_MINGW)
+    invalparamfn old = _set_thread_local_invalid_parameter_handler(_sir_invalidparameter);
+# endif
+    struct _stat st;
+    int ret = _fstat(fd, &st);
+# if !defined(SIR_MSVCRT_MINGW)
+    _set_thread_local_invalid_parameter_handler(old);
+# endif
     bool valid = -1 != ret || EBADF != errno;
     if (-1 == ret)
         _sir_handleerr(errno);
     return valid;
-#else /* __WIN__ */
-    invalparamfn old = _set_thread_local_invalid_parameter_handler(_sir_invalidparameter);
-    intptr_t h = _get_osfhandle(fd);
-    _set_thread_local_invalid_parameter_handler(old);
-    if (INVALID_HANDLE_VALUE == (HANDLE)h) {
-        _sir_seterror(EBADF);
-        return false;
-    }
-    return true;
 #endif
 }
 
