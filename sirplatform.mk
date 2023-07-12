1b0d6ab8e23de15d9f65dbd96182fdd8bc27d412
Jeffrey H. Johnson <trnsz@pobox.com>
2023-07-12 00:33:40 +0000

    GNU Makefile improvements (#112)
    Closes #87
    
    ---------
    
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

---
+++ sirplatform.mk
@@ -0,0 +1,71 @@
+#
+# libsir -- sirplatform.mk
+# https://github.com/aremmell/libsir
+#
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: Copyright (c) 2018-current Ryan M. Lederman
+#
+
+# MinGW-w64 and standard Unix
+ifneq "$(findstring mingw,$(CC))" ""
+  MINGW?=1
+endif
+ifeq ($(MINGW),1)
+  ifneq "$(findstring gcc,$(CC))" ""
+    CFLAGS+=-Wno-unknown-pragmas
+  endif
+  PLATFORM_DLL_EXT=.dll
+  PLATFORM_EXE_EXT=.exe
+  PLATFORM_LIB_EXT=.lib
+  PLATFORM_LIBS=-lshlwapi -lws2_32
+else
+  PLATFORM_DLL_EXT=.so
+  PLATFORM_LIB_EXT=.a
+endif
+
+# Enable MinGW MSVCRT workaround?
+ifeq ($(SIR_MSVCRT_MINGW),1)
+  CFLAGS+=-DSIR_MSVCRT_MINGW
+endif
+
+# Oracle Studio C
+ifneq "$(findstring suncc,$(CC))" ""
+  SUNPRO?=1
+endif
+ifeq ($(SUNPRO),1)
+  CFLAGS+=-fcommon
+  FORTIFY_FLAGS=-U_FORTIFY_SOURCE
+  MMDOPT=-xMMD
+  PTHOPT=-mt=yes
+else
+  FORTIFY_FLAGS=-D_FORTIFY_SOURCE=2
+  MMDOPT=-MMD
+  PTHOPT=-pthread
+endif
+
+# Chibicc and Intel C++ Compiler Classic
+ifneq "$(findstring icc,$(CC))" ""
+  ifneq "$(findstring chibicc,$(CC))" ""
+    FORTIFY_FLAGS=-U_FORTIFY_SOURCE
+    PTHOPT=
+  else
+    INTELC?=1
+  endif
+endif
+ifeq ($(INTELC),1)
+  CFLAGS+=-diag-disable=188
+  CFLAGS+=-diag-disable=191
+  CFLAGS+=-diag-disable=10441
+  CFLAGS+=-diag-disable=10148
+  FORTIFY_FLAGS=-U_FORTIFY_SOURCE
+endif
+
+# NVIDIA HPC SDK C Compiler
+ifneq "$(findstring nvc,$(CC))" ""
+  NVIDIAC?=1
+endif
+ifeq ($(NVIDIAC),1)
+  CFLAGS+=--diag_suppress mixed_enum_type
+  CFLAGS+=--diag_suppress cast_to_qualified_type
+  CFLAGS+=--diag_suppress nonstd_ellipsis_only_param
+endif
