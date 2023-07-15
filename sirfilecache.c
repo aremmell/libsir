b885b68e95c190d8a59ea1bdad917f000800f110
Ryan M. Lederman <lederman@gmail.com>
2023-07-15 10:03:24 -0600

    simplify implementation of path matching

--- sirfilecache.c
+++ sirfilecache.c
@@ -484,35 +484,25 @@ bool _sir_fcache_rem(sirfcache* sfc, sirfileid id) {
 bool _sir_fcache_pred_path(const void* match, sirfile* iter) {
     const char* path = (const char*)match;
 #if !defined(__WIN__)
-    char resolved1[SIR_MAXPATH] = {0};
-    char resolved2[SIR_MAXPATH] = {0};
+    char resolved1[SIR_MAXPATH] = {0}, resolved2[SIR_MAXPATH] = {0};
     struct stat st1 = {0}, st2  = {0};
-    bool stat1 = false, stat2   = false;
 
-    if (NULL == realpath(path, resolved1))
-        _sir_selflog("warning: realpath('%s') failed (%d); resolved: '%s'",
-            path, errno, resolved1);
+    /* realpath "fails" if the file does not exist, but it still puts
+     * the canonical absolute path to the file in the buffer, so ignore
+     * its return value. */
+    (void)realpath(path, resolved1);
+    (void)realpath(iter->path, resolved2);
 
-    stat1 = 0 == stat(resolved1, &st1);
-    if (!stat1)
-        _sir_selflog("warning: stat('%s') failed (%d);", resolved1, errno);
-
-    if (NULL == realpath(iter->path, resolved2))
-        _sir_selflog("warning: realpath('%s') failed (%d); resolved: '%s'",
-            iter->path, errno, resolved2);
-
-    stat2 = 0 == stat(resolved2, &st2);
-    if (!stat2)
-        _sir_selflog("warning: stat('%s') failed (%d)", resolved2, errno);
-
-    /* if we were able to stat both files, then we can do a comparison through
+    /* if we are able to stat both files, then we can do a comparison through
      * the data returned. if not, fall back on trying to match the path by
      * string comparison. */
     bool equal = false;
-    if (stat1 && stat2)
+    if (0 == stat(resolved1, &st1) && 0 == stat(resolved2, &st2)) {
         equal = st1.st_dev == st2.st_dev && st1.st_ino == st2.st_ino;
-    else
+    } else {
+        _sir_selflog("falling back to conversion to canonical path and string compare");
         equal = 0 == strncmp(resolved1, resolved2, SIR_MAXPATH);
+    }
 
     _sir_selflog("returning %d for '%s' == '%s'", equal, resolved1, resolved2);
     return equal;
