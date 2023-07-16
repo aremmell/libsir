047521c8ec74a712b06da8c798b23740e223de51
Ryan M. Lederman <lederman@gmail.com>
2023-07-15 22:58:44 -0600

    finish doxygen comments for plugin_sample.h

--- plugins/sample/plugin_sample.h
+++ plugins/sample/plugin_sample.h
@@ -41,22 +41,66 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD ul_reason_for_call, LPVOID reserved)
  * any other functions are probed or called.
  *
  * The plugin must fill out the information in the `info` structure in a way
- * satisfactory to libsir, or it will immediately be unloaded.
+ * satisfactory to libsir, or it will immediately be unloaded:
  *
- * - `iface_ver` should be set to `SIR_PLUGIN_VCURRENT`; if the interface
- * version does not match libsir's expectation, the plugin will be unloaded.
- * - `char*` members must point at static memory that will not go out of scope
- * when the function exits. Do not allocate heap memory for these properties;
- * they will be leaked.
+ * - all members of the ::sirplugininfo structure are required to be set by the
+ *   plugin.
+ * - `iface_ver` must be set to `SIR_PLUGIN_VCURRENT`; if the interface version
+ *   does not match libsir's expectation, the plugin will be unloaded.
+ * - `char*` members must be set to valid strings, and must point at static memory
+ *   that will not go out of scope when the function exits. Do not allocate heap
+ *   memory for these properties; they will be leaked.
+ * - `levels` must be a valid ::sir_level bitmask.
+ * - `opts` must be a valid ::sir_option bitmask.
+ *
+ * @note It is not possible to change these settings once chosen, so ensure that
+ * they are correct.
+ *
+ * @see ::sir_level
+ * @see ::sir_option
+ * @see ::default
  *
  * @param   info Pointer to a ::sir_plugininfo structure to be initialized by
- * the plugin.
+ *               the plugin.
  * @returns bool `true` if the `info` structure was successfully initialized,
- * `false` otherwise.
+ *               `false` otherwise. If `false`, the plugin will be unloaded.
  */
 PLUGIN_EXPORT bool sir_plugin_query(sir_plugininfo* info);
+
+/**
+ * @brief Called by libsir after the plugin is queried and successfully validated.
+ *
+ * The plugin should only at the time of this call begin allocating resources and
+ * initializing its internal state.
+ *
+ * @returns bool `true` if the plugin successfully initialized its internal
+ *                state, `false` otherwise.
+ */
 PLUGIN_EXPORT bool sir_plugin_init(void);
+
+/**
+ * @brief Called by libsir when a message is dispatched on any level whose bit
+ * was set in the `levels` bitmask of the ::sir_plugininfo structure when
+ * ::sir_plugin_query was called.
+ *
+ * The `message` string will be pre-formatted according to the ::sir_option set
+ * in the `opts` bitmask of the ::sir_plugininfo structure.
+ *
+ * @param   level   The ::sir_level of the message being dispatched.
+ * @param   message A string containing the pre-formatted message being dispatched.
+ * @returns bool    `true` if the message was successfully processed, `false`
+ *                  otherwise.
+ */
 PLUGIN_EXPORT bool sir_plugin_write(sir_level level, const char* message);
+
+/**
+ * @brief Called by libsir when the plugin is about to be unloaded.
+ *
+ * The plugin should immediately begin releasing allocated resources and
+ * resetting its internal state.
+ *
+ * @returns bool `true` if the plugin cleaned up successfully, `false` otherwise.
+ */
 PLUGIN_EXPORT bool sir_plugin_cleanup(void);
 
 #endif /* !_SIR_PLUGIN_SAMPLE_H_INCLUDED */
