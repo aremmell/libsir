0d8387b8607e411b3a9fb12b977c12dda4d32efa
Ryan M. Lederman <lederman@gmail.com>
2023-07-17 21:53:41 -0600

    bump version 2.2.1-dev

--- include/sir/version.h
+++ include/sir/version.h
@@ -35,13 +35,13 @@
 # define SIR_VERSION_MINOR 2
 
 /** The current libsir patch version component. */
-# define SIR_VERSION_PATCH 0
+# define SIR_VERSION_PATCH 1
 
 /** 1 if this is a release version of libsir, 0 otherwise. */
-# define SIR_VERSION_IS_RELEASE 1
+# define SIR_VERSION_IS_RELEASE 0
 
 /** The current libsir version suffix. */
-# define SIR_VERSION_SUFFIX ""
+# define SIR_VERSION_SUFFIX "-dev"
 
 /** The current libsir version as a number. */
 # define SIR_VERSION_HEX ((SIR_VERSION_MAJOR << 16) | \
