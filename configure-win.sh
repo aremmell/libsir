81e39a80d86d3eee088166b106fa5d2345a20c68
Ryan M. Lederman <lederman@gmail.com>
2023-07-24 06:54:06 -0600

    trying shell script

---
+++ configure-win.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+echo "Exporting environment variables"
+export VCTargetsPath="/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Microsoft/VC/v170"
+echo "Running MSBuild..."
+/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Rebuild -p:Config=Release -p:Platform=x64 || {
+    echo "Command failed with exit code $?!"
+}
+
+echo "Looking for Microsoft.Cpp.Default.props files..."
+find /c/ -name Microsoft.Cpp.Default.props -type f -print 2>/dev/null
