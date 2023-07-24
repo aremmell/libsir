87110039abec513fb61880f1a1348490ca9ce78d
Ryan M. Lederman <lederman@gmail.com>
2023-07-24 09:46:56 -0600

    Configuration=Release

--- configure-win.sh
+++ configure-win.sh
@@ -3,9 +3,6 @@
 echo "Exporting environment variables..."
 export VCTargetsPath="/c/Program Files/Microsoft Visual Studio/2022/Community/Msbuild/Microsoft/VC/v170/"
 echo "Running MSBuild..."
-/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Rebuild -p:Config=Release -p:Platform=x64 || {
-    echo "Command failed with exit code $?!"
+/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Rebuild -p:Configuration=Release -p:Platform=x64 || {
+    echo "Failed to build libsir!"
 }
-
-#echo "Looking for Microsoft.Cpp.Default.props files..."
-#find /c/ -name Microsoft.Cpp.Default.props -type f -print 2>/dev/null
