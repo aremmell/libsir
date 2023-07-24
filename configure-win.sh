03188ac8cb2d913957939555bda3974002ee43f1
Ryan M. Lederman <lederman@gmail.com>
2023-07-24 10:01:39 -0600

    triggering new travis build

--- configure-win.sh
+++ configure-win.sh
@@ -4,5 +4,5 @@ echo "Exporting environment variables..."
 export VCTargetsPath="/c/Program Files/Microsoft Visual Studio/2022/Community/Msbuild/Microsoft/VC/v170/"
 echo "Running MSBuild..."
 /c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Rebuild -p:Configuration=Release -p:Platform=x64 || {
-    echo "Failed to build libsir!"
+    echo "!! Failed to build libsir !!"
 }
