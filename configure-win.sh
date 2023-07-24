87fe77aca77c5c1c53453904d69c6677787299bb
Ryan M. Lederman <lederman@gmail.com>
2023-07-24 17:12:54 -0600

    try Build instead of Rebuild

--- configure-win.sh
+++ configure-win.sh
@@ -3,6 +3,6 @@
 echo "Exporting environment variables..."
 export VCTargetsPath="/c/Program Files/Microsoft Visual Studio/2022/Community/Msbuild/Microsoft/VC/v170/"
 echo "Running MSBuild..."
-/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Rebuild -p:Configuration=Release -p:Platform=x64 || {
+/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Build -p:Configuration=Release -p:Platform=x64 || {
     echo "!! Failed to build libsir !!"
 }
