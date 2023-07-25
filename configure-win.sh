76a0a092efcd6edb1ca20e019635180fff7bda87
Ryan M. Lederman <lederman@gmail.com>
2023-07-24 19:43:01 -0600

    delete script

--- configure-win.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-echo "Exporting environment variables..."
-export VCTargetsPath="/c/Program Files/Microsoft Visual Studio/2022/Community/Msbuild/Microsoft/VC/v170/"
-echo "Running MSBuild..."
-/c/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe ./msvs/libsir.sln -t:Build -p:Configuration=Release -p:Platform=x64 || {
-    echo "!! Failed to build libsir !!"
-}
