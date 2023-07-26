bcc1a075c62dc47a9acf53ce6dc2f4b6f4ba4e16
Jeffrey H. Johnson <trnsz@pobox.com>
2023-07-26 14:46:40 -0400

    Test for GitLab CI
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

---
+++ .gitlab-ci/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2018-current Ryan M. Lederman
+
+printf '%s\n' "Sanity check ..."
+test -f Dockerfile ||
+  {
+    printf '%s\n' "ERROR: No Dockerfile in current working directory."
+    exit 1
+  }
+
+printf '%s\n' "Finding docker ..."
+command -v docker > /dev/null 2>&1 ||
+  {
+    printf '%s\n' "ERROR: No docker command found in PATH."
+    exit 1
+  }
+
+printf '%s\n' "Testing docker ..."
+docker run --rm -it "hello-world:latest" ||
+  {
+    printf '%s\n' "ERROR: Docker cannot run containers."
+    exit 1
+  }
+
+printf '%s\n' "Building image ..."
+docker build --no-cache --squash -t registry.gitlab.com/libsir/libsir/helper:latest .
+
+printf '%s\n' "Complete."
