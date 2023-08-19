7326d306358f9c1e5600ff79ec6951698c1178ee
Jeffrey H. Johnson <trnsz@pobox.com>
2023-08-19 14:02:51 -0400

    Avoid potential integer overflow on 32-bit platfoms
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- include/sir/helpers.h
+++ include/sir/helpers.h
@@ -328,7 +328,7 @@ uint32_t FNV32_1a(const uint8_t* data, size_t len) {
     uint32_t hash = 2166136261U;
     for (size_t n = 0; n < len; n++) {
         hash ^= (uint32_t)data[n];
-        hash *= 16777619UL;
+        hash = (uint32_t)(hash * 16777619ULL);
     }
     return hash;
 }
