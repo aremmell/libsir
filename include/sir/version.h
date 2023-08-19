b931ab776ab146e043e128512a98ef23da4f543f
Ryan M. Lederman <lederman@gmail.com>
2023-08-18 23:25:15 -0600

    suffx="-dev", is_release=0

--- include/sir/version.h
+++ include/sir/version.h
@@ -38,10 +38,10 @@
 # define SIR_VERSION_PATCH 2
 
 /** 1 if this is a release version of libsir, 0 otherwise. */
-# define SIR_VERSION_IS_RELEASE 1
+# define SIR_VERSION_IS_RELEASE 0
 
 /** The current libsir version suffix. */
-# define SIR_VERSION_SUFFIX ""
+# define SIR_VERSION_SUFFIX "-dev"
 
 /** The current libsir version as a number. */
 # define SIR_VERSION_HEX ((SIR_VERSION_MAJOR << 16) | \
