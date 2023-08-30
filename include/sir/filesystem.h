afc57395bf0e64daecd3130f78e724590c5e9893
Ryan M. Lederman <lederman@gmail.com>
2023-08-29 21:54:08 -0600

    only declare _sir_readlink !__WIN__

--- include/sir/filesystem.h
+++ include/sir/filesystem.h
@@ -67,6 +67,7 @@ bool _sir_getrelbasepath(const char* restrict path, bool* restrict relative,
 
 bool _sir_deletefile(const char* restrict path);
 
+# if !defined(__WIN__)
 /* suppress unconditional warning from Flawfinder about the use of
  * the readlink function, which is prone to pathname race conditions. */
 static inline
@@ -74,6 +75,7 @@ ssize_t _sir_readlink(const char* restrict path, char* restrict buf, size_t bufs
 {
     return readlink(path, buf, bufsize); /* flawfinder: ignore */
 }
+# endif
 
 # if defined(_AIX)
 int _sir_aixself(char *buffer, size_t *size);
