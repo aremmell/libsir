75a7de2fbb862dd31cbc7a74dee50d058db46eba
Jeffrey H. Johnson <trnsz@pobox.com>
2023-09-01 15:39:36 -0400

    Don't use C11 atomics on Win32 DMD ImportC
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- include/sir/platform.h
+++ include/sir/platform.h
@@ -235,7 +235,8 @@ int pthread_getname_np(pthread_t thread, char* buffer, size_t length);
 
 #  undef __HAVE_ATOMIC_H__
 
-#  if defined(_MSC_VER) && _MSC_VER >= 1933 && !defined(__cplusplus)
+#  if defined(_MSC_VER) && _MSC_VER >= 1933 && \
+      !defined(__IMPORTC__) && !defined(__cplusplus)
 #   include <stdatomic.h>
 #   define __HAVE_ATOMIC_H__
 #  endif
