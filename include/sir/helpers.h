68a23de91e7ae705ae1f2bbb22041691ca3dd4b3
Jeffrey H. Johnson <trnsz@pobox.com>
2023-09-01 15:59:45 -0400

    Opitimize the suppression for GCC Analayzer
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- include/sir/helpers.h
+++ include/sir/helpers.h
@@ -349,10 +349,8 @@ uint32_t FNV32_1a(const uint8_t* data, size_t len) {
  */
 # define _sir_snprintf_trunc(dst, size, ...) \
     do { \
-      static volatile size_t _n = size; \
-      volatile int _sir_snprintf_trunc_toss = snprintf(dst, _n, __VA_ARGS__); \
-      (void)_sir_snprintf_trunc_toss; \
-      (void)_n; \
+      volatile size_t _n = size; \
+      if (!snprintf(dst, _n, __VA_ARGS__)) { (void)_n; }; \
     } while (false)
 
 /**
