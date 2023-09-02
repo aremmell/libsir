6edfe927e3cdeb08865744515cb4236a9465e39e
Ryan M. Lederman <lederman@gmail.com>
2023-09-02 15:23:04 -0600

    revert inlining of _sir_strn[cat|cpy]

--- include/sir/helpers.h
+++ include/sir/helpers.h
@@ -302,53 +302,15 @@ bool _sir_strsame(const char* lhs, const char* rhs, size_t count) {
  * Wrapper for str[n,l]cpy/strncpy_s. Determines which one to use
  * based on preprocessor macros.
  */
-static inline
-int _sir_strncpy(char* restrict dest, size_t destsz, const char* restrict src,
-    size_t count) {
-    if (_sir_validptr(dest) && _sir_validstr(src)) {
-# if defined(__HAVE_STDC_SECURE_OR_EXT1__)
-        int ret = strncpy_s(dest, destsz, src, count);
-        if (0 != ret) {
-            (void)_sir_handleerr(ret);
-            return -1;
-        }
-        return 0;
-# else
-        SIR_UNUSED(count);
-        size_t cpy = strlcpy(dest, src, destsz);
-        SIR_ASSERT_UNUSED(cpy < destsz, cpy);
-        return 0;
-# endif
-    }
-
-    return -1;
-}
+int _sir_strncpy(char* restrict dest, size_t destsz,
+    const char* restrict src, size_t count);
 
 /**
  * Wrapper for str[n,l]cat/strncat_s. Determines which one to use
  * based on preprocessor macros.
  */
-static inline
-int _sir_strncat(char* restrict dest, size_t destsz, const char* restrict src,
-    size_t count) {
-    if (_sir_validptr(dest) && _sir_validstr(src)) {
-# if defined(__HAVE_STDC_SECURE_OR_EXT1__)
-        int ret = strncat_s(dest, destsz, src, count);
-        if (0 != ret) {
-            (void)_sir_handleerr(ret);
-            return -1;
-        }
-        return 0;
-# else
-        SIR_UNUSED(count);
-        size_t cat = strlcat(dest, src, destsz);
-        SIR_ASSERT_UNUSED(cat < destsz, cat);
-        return 0;
-# endif
-    }
-
-    return -1;
-}
+int _sir_strncat(char* restrict dest, size_t destsz,
+    const char* restrict src, size_t count);
 
 /**
  * Wrapper for fopen/fopen_s. Determines which one to use
