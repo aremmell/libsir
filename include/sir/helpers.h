c447d3946657308b568b9177e21abf895289711b
Ryan M. Lederman <lederman@gmail.com>
2023-09-05 07:34:03 -0600

    remove unused macro

--- include/sir/helpers.h
+++ include/sir/helpers.h
@@ -155,10 +155,6 @@ bool _sir_setbitslow(uint32_t* flags, uint32_t set) {
  * bool as an operand for that operator. */
 # define _sir_andeql(b, expr) ((b) = (b) && (expr))
 
-/** Effectively performs b |= expr without the linter warnings about using
- * bool as an operand for that operator. */
-# define _sir_oreql(b, expr) ((b) = (b) || (expr))
-
 /** Calls free and sets the pointer to NULL. */
 void __sir_safefree(void** pp);
 
