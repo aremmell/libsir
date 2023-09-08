ff2bb9e592850f701776db19c22e3b532eba4b11
Jeffrey H. Johnson <trnsz@pobox.com>
2023-09-08 16:51:48 -0400

    Begin 2.2.4-dev
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- include/sir.h
+++ include/sir.h
@@ -7,7 +7,7 @@
  *
  * @author    Ryan M. Lederman \<lederman@gmail.com\>
  * @date      2018-2023
- * @version   2.2.3
+ * @version   2.2.4
  * @copyright The MIT License (MIT)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -751,7 +751,7 @@ bool sir_syslogcat(const char* category);
  * **Example:**
  *
  * ~~~
- * 2.2.3-dev
+ * 2.2.4-dev
  * ~~~
  *
  * @return const char* The current libsir version string.
