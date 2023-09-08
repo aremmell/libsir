3c5ec2c8756d130adce75d34dfd15a62313fa6ce
Jeffrey H. Johnson <trnsz@pobox.com>
2023-09-08 15:31:53 -0400

    Add SIR_DPSPRINTF=1 to CI mcmb
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- .lint.sh
+++ .lint.sh
@@ -99,6 +99,7 @@ test_mcmb()
              SIR_DEBUG=1 \
              SIR_NO_SYSTEM_LOGGERS=1 \
              SIR_NO_PLUGINS=1 \
+             SIR_DPSPRINTF=1 \
              SIR_SELFLOG=1"
   # shellcheck disable=SC2090
   printf '%s\n' "exported SIR_OPTIONS=' ${SIR_OPTIONS} '" | tr -s ' '
