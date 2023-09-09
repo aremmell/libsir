b3a3530c7ae0259caa8784c6d3697a37928408dd
Ryan M. Lederman <lederman@gmail.com>
2023-09-09 01:09:21 -0600

    add ability to turn on SIR_SELFLOG

--- tmp_make_c++.sh
+++ tmp_make_c++.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
+selflog=true
+CFLAGS="--std=c++20"
+
+test ${selflog} = true && {
+    make_env="env SIR_SELFLOG=1"
+    CFLAGS="${CFLAGS} -DSIR_SELFLOG"
+}
+
 printf 'Making libsir static library...\n'
-make static || {
+${make_env} make static || {
     printf 'Failed to compile; exiting!'
     exit 1
 }
@@ -10,6 +18,7 @@ printf 'Ensuring build/bin exists...\n'
 test -d build/bin || mkdir -p build/bin
 
 printf 'Compiling C++ test rig...\n'
-c++ --std=c++20 -c tests/tests.cc -o build/obj/tests.cc.o -Iinclude && \
+
+c++ -c tests/tests.cc -o build/obj/tests.cc.o ${CFLAGS} -Iinclude && \
 c++ -o build/bin/sirtests++ build/obj/tests.cc.o -Lbuild/lib -lsir_s && \
 printf 'Created build/bin/sirtests++\n'
