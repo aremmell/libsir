de1dbdb53ad6080db5c2dd90a0a2445188f26b24
Ryan M. Lederman <lederman@gmail.com>
2023-09-08 22:20:14 -0600

    change case, add constants/enums

--- swig/python/sirtest.py
+++ swig/python/sirtest.py
@@ -4,35 +4,35 @@ import sir
 
 if __name__ == "__main__":
     s = sir.InitData()
-    if not sir.MakeInit(s):
-        print("Error: MakeInit failed")
+    if not sir.makeinit(s):
+        print("Error: makeinit failed")
         exit(1)
 
     s.name = "Python!"
-    s.d_stdout.levels = sir.SIRL_ALL
-    s.d_stdout.opts   = sir.SIRO_NOHOST
-    s.d_syslog.levels = sir.SIRL_NONE
+    s.d_stdout.levels = sir.LVL_ALL
+    s.d_stdout.opts   = sir.OPT_NOHOST
+    s.d_syslog.levels = sir.LVL_NONE
 
-    if not sir.Init(s):
-        print("Error: Init failed")
+    if not sir.init(s):
+        print("Error: init failed")
         exit(1)
 
-    sir.Debug('This is a test of the %s system. Testing %d' % ('libsir', 123))
-    sir.Debug('Adding log file %s' % ('sir-python.log'))
+    sir.debug('This is a test of the %s system. Testing %d' % ('libsir', 123))
+    sir.debug('Adding log file %s' % ('sir-python.log'))
 
-    f = sir.AddFile('sir-python.log', sir.SIRL_ALL, sir.SIRO_ALL)
+    f = sir.addfile('sir-python.log', sir.LVL_ALL, sir.OPT_ALL)
     if f == 0:
-        print("Error: AddFile failed")
+        print("Error: addfile failed")
         exit(1)
 
-    sir.Info('Testing info level')
-    sir.Notice('Testing notice level')
-    sir.Warn('Testing warning level')
-    sir.Error('Testing error level')
-    sir.Crit('Testing critical level')
-    sir.Alert('Testing alert level')
-    sir.Emerg('Testing emergency level')
-    sir.Info('Cleaning up...')
+    sir.info('Testing info level')
+    sir.notice('Testing notice level')
+    sir.warn('Testing warning level')
+    sir.error('Testing error level')
+    sir.crit('Testing critical level')
+    sir.alert('Testing alert level')
+    sir.emerg('Testing emergency level')
+    sir.info('Cleaning up...')
 
-    sir.RemFile(f)
-    sir.Cleanup()
+    sir.remfile(f)
+    sir.cleanup()
