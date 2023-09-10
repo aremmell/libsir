d5f4111ddd2af58d35f2268eb3071b258eb2986e
Ryan M. Lederman <lederman@gmail.com>
2023-09-10 03:36:51 -0600

    WIP C++ test rig

--- tests/tests++.cc
+++ tests/tests++.cc
@@ -1,84 +1,164 @@
-#include "sir.hh"
+#include "tests++.hh"
 #include <cstdlib>
 #include <cstdio>
 
 using namespace std;
 using namespace sir;
+using namespace sir::tests;
+
+static const test_vector tests = {
+    {raii_init_cleanup},
+    {manual_init_cleanup},
+    {error_handling},
+    {std_format},
+    {boost_format},
+    {fmt_format},
+    {std_iostream_format}
+};
 
 int main(int argc, char** argv) {
     SIR_UNUSED(argc);
     SIR_UNUSED(argv);
 
-    {
+    try {
         default_logger logger;
+        
+        /* logger.test_stream << "ABCDEF";
+        logger.test_stream << "GHIJKLMNOPQRSTUVWXYZ" << 1234 << EOF; */
 
-        logger.debug("C++ RAII logger initialized via constructor");
-        logger.info("Testing info level");
-        logger.notice("Testing notice level");
-        logger.warn("Testing warning level");
-        logger.error("Testing error level");
-        logger.crit("Testing critical level");
-        logger.alert("Testing alert level");
-        logger.emerg("Testing emergency level");
-
-        logger.debug("Testing get_error by doing something stupid...");
-        logger.debug(NULL);
-
-        auto [ code, message ] = logger.get_error();
-        logger.debug("Error info: code = %u, message = '%s'", code,
-            message.c_str());
-
-        /* std:: format. */
-#if defined(__SIR_HAVE_STD_FORMAT__)
-        logger.debug_std("Testing {} {}", "debug", "std::format");
-        logger.info_std("Testing {} {}", "info", "std::format");
-        logger.notice_std("Testing {} {}", "notice", "std::format");
-        logger.warn_std("Testing {} {}", "warn", "std::format");
-        logger.error_std("Testing {} {}", "error", "std::format");
-        logger.crit_std("Testing {} {}", "crit", "std::format");
-        logger.alert_std("Testing {} {}", "alert", "std::format");
-        logger.emerg_std("Testing {} {}", "emerg", "std::format");
-#endif // !__SIR_HAVE_STD_FORMAT__
-
-        /* boost::format. */
-#if defined(__SIR_HAVE_BOOST_FORMAT__)
-        logger.debug_boost(boost::format("Testing %1% %2%") % "debug" % "boost::format");
-        logger.info_boost(boost::format("Testing %1% %2%") % "info" % "boost::format");
-        logger.notice_boost(boost::format("Testing %1% %2%") % "notice" % "boost::format");
-        logger.warn_boost(boost::format("Testing %1% %2%") % "warn" % "boost::format");
-        logger.error_boost(boost::format("Testing %1% %2%") % "error" % "boost::format");
-        logger.crit_boost(boost::format("Testing %1% %2%") % "crit" % "boost::format");
-        logger.alert_boost(boost::format("Testing %1% %2%}") % "alert" % "boost::format");
-        logger.emerg_boost(boost::format("Testing %1% %2%") % "emerg" % "boost::format");
-#endif // !__SIR_HAVE_BOOST_FORMAT__
-
-        /* fmt */
-#if defined(__SIR_HAVE_FMT_FORMAT__)
-        logger.debug_fmt("Testing {} {}", "debug", "fmt");
-        logger.info_fmt("Testing {} {}", "info", "fmt");
-        logger.notice_fmt("Testing {} {}", "notice", "fmt");
-        logger.warn_fmt("Testing {} {}", "warn", "fmt");
-        logger.error_fmt("Testing {} {}", "error", "fmt");
-        logger.crit_fmt("Testing {} {}", "crit", "fmt");
-        logger.alert_fmt("Testing {} {}", "alert", "fmt");
-        logger.emerg_fmt("Testing {} {}", "emerg", "fmt");
-#endif // !__SIR_HAVE_FMT_FORMAT__
-
-        logger.debug("sir_cleanup() will be called by the destructor");
+    } catch (std::exception& ex) {
+        printf("ERROR: caught std::exception: '%s'!\n", ex.what());
+        return EXIT_FAILURE;
+    } catch (...) {
+        printf("ERROR: caught unknown exception!\n");
+        return EXIT_FAILURE;
     }
-
-    {
-        sir::logger<false, default_init_policy, default_adapter> logger2;
-        if (!logger2.init())
-            SIR_ASSERT(false);
-
-        logger2.debug("Manually initialized using default_init_policy");
-        logger2.debug("Will not clean up in the destructor; cleaning up manually");
-
-        if (!logger2.cleanup())
-            SIR_ASSERT(false);
-    }
-
 
     return EXIT_SUCCESS;
+}
+
+bool sir::tests::raii_init_cleanup() {
+    bool pass = true;
+
+    /* scope a RAII logger object and test whether or not it successfully
+     * initializes and cleans up libsir. */
+    if (true)
+    {
+        default_logger log;
+
+        pass &= log.debug("C++ RAII logger initialized via constructor");
+        pass &= log.info("Testing info level");
+        pass &= log.notice("Testing notice level");
+        pass &= log.warn("Testing warning level");
+        pass &= log.error("Testing error level");
+        pass &= log.crit("Testing critical level");
+        pass &= log.alert("Testing alert level");
+        pass &= log.emerg("Testing emergency level");
+        pass &= log.debug("Cleanup will be performed via destructor");
+    }
+
+    pass &= !sir_cleanup();
+
+    return pass;
+}
+
+bool sir::tests::manual_init_cleanup() {
+    bool pass = true;
+
+    logger<false, default_init_policy, default_adapter> log;
+    pass &= log.init();
+
+    pass &=log.debug("Manually initialized using default_init_policy");
+    pass &=log.debug("Will not clean up in the destructor; cleaning up manually");
+
+    pass &= log.cleanup();
+
+    return pass;
+}
+
+bool sir::tests::error_handling() {
+    bool pass = true;
+
+    default_logger log;
+
+    pass &= log.debug("Testing get_error by doing something stupid...");
+    pass &= !log.debug(nullptr);
+
+    const auto [ code, message ] = log.get_error();
+    pass &= log.debug("Error info: code = %u, message = '%s'", code,
+        message.c_str());
+
+    return pass;
+}
+
+bool sir::tests::std_format() {
+    bool pass = true;
+
+    /* std:: format. */
+#if defined(__SIR_HAVE_STD_FORMAT__)
+    default_logger log;
+    log.debug_std("Testing {} {}", "debug", "std::format");
+    log.info_std("Testing {} {}", "info", "std::format");
+    log.notice_std("Testing {} {}", "notice", "std::format");
+    log.warn_std("Testing {} {}", "warn", "std::format");
+    log.error_std("Testing {} {}", "error", "std::format");
+    log.crit_std("Testing {} {}", "crit", "std::format");
+    log.alert_std("Testing {} {}", "alert", "std::format");
+    log.emerg_std("Testing {} {}", "emerg", "std::format");
+#else
+    printf("\tstd::format support not enabled; skipping\n");
+    pass = true;
+#endif // !__SIR_HAVE_STD_FORMAT__
+
+    return pass;
+}
+
+bool sir::tests::boost_format() {
+    bool pass = true;
+
+    /* boost::format. */
+#if defined(__SIR_HAVE_BOOST_FORMAT__)
+    default_logger log;
+    log.debug_boost(boost::format("Testing %1% %2%") % "debug" % "boost::format");
+    log.info_boost(boost::format("Testing %1% %2%") % "info" % "boost::format");
+    log.notice_boost(boost::format("Testing %1% %2%") % "notice" % "boost::format");
+    log.warn_boost(boost::format("Testing %1% %2%") % "warn" % "boost::format");
+    log.error_boost(boost::format("Testing %1% %2%") % "error" % "boost::format");
+    log.crit_boost(boost::format("Testing %1% %2%") % "crit" % "boost::format");
+    log.alert_boost(boost::format("Testing %1% %2%}") % "alert" % "boost::format");
+    log.emerg_boost(boost::format("Testing %1% %2%") % "emerg" % "boost::format");
+#else
+    printf("\tboost::format support not enabled; skipping\n");
+    pass = true;
+#endif // !__SIR_HAVE_BOOST_FORMAT__
+
+    return pass;
+}
+
+bool sir::tests::fmt_format() {
+    bool pass = true;
+
+    /* fmt */
+#if defined(__SIR_HAVE_FMT_FORMAT__)
+    default_logger log;
+    log.debug_fmt("Testing {} {}", "debug", "fmt");
+    log.info_fmt("Testing {} {}", "info", "fmt");
+    log.notice_fmt("Testing {} {}", "notice", "fmt");
+    log.warn_fmt("Testing {} {}", "warn", "fmt");
+    log.error_fmt("Testing {} {}", "error", "fmt");
+    log.crit_fmt("Testing {} {}", "crit", "fmt");
+    log.alert_fmt("Testing {} {}", "alert", "fmt");
+    log.emerg_fmt("Testing {} {}", "emerg", "fmt");
+#else
+    printf("\tfmt::format support not enabled; skipping\n");
+    pass = true;
+#endif // !__SIR_HAVE_FMT_FORMAT__
+
+    return pass;
+}
+
+bool sir::tests::std_iostream_format() {
+    bool pass = true;
+
+    return pass;
 }
---
+++ tests/tests++.hh
@@ -0,0 +1,100 @@
+/*
+ * tests++.hh
+ *
+ * Author:    Ryan M. Lederman <lederman@gmail.com>
+ * Copyright: Copyright (c) 2018-2023
+ * Version:   2.2.4
+ * License:   The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef _TESTSXX_HH_INCLUDED
+# define _TESTSXX_HH_INCLUDED
+
+# include "sir.hh"
+# include <vector>
+
+/**
+ * @addtogroup tests
+ * @{
+ */
+
+/** Contains the C++ tests and test rig implementation. */
+namespace sir::tests
+{
+    /** Vector of unit test function pointers. */
+    typedef std::vector<bool(*)()> test_vector;
+
+    /**
+     * @test raii_init_cleanup
+     * @brief Ensure that RAII loggers are functioning properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool raii_init_cleanup();
+
+    /**
+     * @test manual_init_cleanup
+     * @brief Ensure that manually initializing and cleaning up is working properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool manual_init_cleanup();
+
+    /**
+     * @test error_handling
+     * @brief Ensure that the C++ wrapper properly handles errors from libsir.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool error_handling();
+
+    /**
+     * @test std_format
+     * @brief If std::format support is enabled, ensures that it is working properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool std_format();
+
+    /**
+     * @test boost_format
+     * @brief If boost::format support is enabled, ensures that it is working properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool boost_format();
+
+    /**
+     * @test fmt_format
+     * @brief If fmt::format support is enabled, ensures that it is working properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool fmt_format();
+
+    /**
+     * @test std_iostream_format
+     * @brief If std::iostream support is enabled, ensures that it is working properly.
+     * @returns `true` if the test succeeded, `false` otherwise.
+     */
+    bool std_iostream_format();
+
+    /**
+     * @ test
+     * @ brief
+     * @ returns `true` if the test succeeded, `false` otherwise.
+     */
+    //bool ();
+} // !namespace sir::tests
+
+#endif // !_TESTSXX_HH_INCLUDED
