de9c642b77825a96787d5ca8be938247bfae9034
Ryan M. Lederman <lederman@gmail.com>
2023-09-26 00:30:41 -0600

    [hopefully] finish logging provider setup script

--- msvs/compile_man.ps1
+++ msvs/compile_man.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: Copyright (c) 2018-current Ryan M. Lederman
 
@@ -6,11 +8,11 @@ try {
     . ("$dir\util.ps1")
 } catch {
     write-host "failed to load required PowerShell scripts!"
+    exit 1
 }
 
 $machine  = "x64"
 $filename = "sir_wineventlog"
-$install_ps1 = "$dir\install_man.ps1"
 
 # generates .h and .rc files
 echo-blue "generating $filename.h and $filename.rc..."
@@ -44,23 +46,49 @@ if (!$?) {
     exit 1
 }
 
-# prompt the user, letting them know the next step requires elevation
-while ($True) {
-    $answer = read-host "the logging provider must now be installed, which requires" `
-        "elevation. proceed [y/n]? "
+# get the provider name from the user
+$provider = ""
 
-    if ($answer -ieq "n") {
-        echo-blue "run $install_ps1 as administrator to complete installation"
-        exit 0
-    }
-
-    if ($answer -ieq "y") {
-        break
-    }
-
-    echo-red "the only valid responses are 'y' or 'n'"
+while ($provider -eq "") {
+    $provider = read-host "enter the name that should appear in the event viewer"
 }
 
+# make a copy of the input manifest and rename it
+echo-blue "making a copy of $filename.man at $provider.man..."
+copy-item -Force .\$filename.man ".\$provider.man"
+if (!$?) {
+    echo-red "failed to copy .\$filename.man to .\$provider.man!"
+    exit 1
+}
 
+# replace the provider name with the one from the command line
+echo-blue "replacing provider name with '$provider'..."
+(Get-Content ".\$provider.man") -replace 'provider name="([\w-.]+)"',
+    "provider name=`"${provider}`"" | Set-Content ".\$provider.man"
+if (!$?) {
+    echo-red "failed to replace provider name with '$provider' in .\$provider.man!"
+    exit 1
+}
 
-echo-green "done. all commands completed successfully."
+# copy the DLL to the system directory
+$target_dir = "${env:SystemRoot}\System32"
+
+echo-blue "copying .\${filename}_${machine}.dll to $target_dir..."
+copy-item -Force ".\${filename}_${machine}.dll" "${target_dir}\${filename}_${machine}.dll"
+if (!$?) {
+    echo-red "failed to copy .\${filename}_${machine}.dll to $target_dir!"
+    exit 1
+}
+
+# register the manifest, replacing the file names
+echo-blue "registering $provider.man..."
+wevtutil im /rf:${target_dir}\${filename}_${machine}.dll `
+    /mf:${target_dir}\${filename}_${machine}.dll `
+    /pf:${target_dir}\${filename}_${machine}.dll ".\$provider.man"
+if (!$?) {
+    echo-red "failed to register logging provider!"
+    exit 1
+}
+
+echo-green ("sucessfully registered logging provider. to unregister, run" +
+    " 'wevtutil um $provider.man' `ndone.")
