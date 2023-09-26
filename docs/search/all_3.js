92c53178432700dc6860d8497a325219a29ef83e
Ryan M. Lederman <lederman@gmail.com>
2023-09-26 08:32:01 -0600

    update docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -6,7 +6,7 @@ var searchData=
   ['category_3',['category',['../group__publictypes.html#ace79b2f2bd4d08d0c6e4151dafb3266b',1,'sir_syslog_dest']]],
   ['changelog_4',['Changelog',['../md_docs_2sources_2changelog.html',1,'']]],
   ['cleanup_5',['cleanup',['../structsir__pluginifacev1.html#a17698cba1d7ed214a11774078526c615',1,'sir_pluginifacev1']]],
-  ['code_6',['code',['../namespacesir.html#a4859a24ff453b0a05a7824a824e7117a',1,'sir::error']]],
+  ['code_6',['code',['../structsir_1_1error.html#a4859a24ff453b0a05a7824a824e7117a',1,'sir::error::code'],['../group__publictypes.html#a1b19b7713ae46decdf5c2f25afe6db2b',1,'sir_errorinfo::code']]],
   ['color_7',['COLOR',['../ansimacros_8h.html#a1ebf33726fd433028ef8313fcc97fe02',1,'ansimacros.h']]],
   ['cond_8',['cond',['../structsir__threadpool.html#a982e2a7ec6c7715b055df6a4b7f44173',1,'sir_threadpool']]],
   ['condition_2eh_9',['condition.h',['../condition_8h.html',1,'']]],
