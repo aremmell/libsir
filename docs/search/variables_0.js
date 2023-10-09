058c184225cbc22b9ba4294c41d575a1ed3ddffb
Jeffrey H. Johnson <trnsz@pobox.com>
2023-10-09 12:20:59 -0400

    Regenerate docs
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5fstate_0',['_state',['../group__publictypes.html#aad08e3b5649d10481b87bf507cacb1a5',1,'sir_syslog_dest']]]
+  ['_5fstate_0',['_state',['../group__publictypes.html#adb7f555cb381826a0aedfdbb58e96bd1',1,'sir_syslog_dest']]]
 ];
