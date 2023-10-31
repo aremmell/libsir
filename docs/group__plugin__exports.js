d2b61cdf19c91d17b0e1e76831a191ccb2d71698
Jeffrey H. Johnson <trnsz@pobox.com>
2023-10-31 08:53:28 -0400

    Clean out old Doxygen output from moved and renamed files, etc.
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- docs/group__plugin__exports.js
+++
@@ -1,7 +0,0 @@
-var group__plugin__exports =
-[
-    [ "sir_plugin_query", "group__plugin__exports.html#ga938b693a1eae42f7d93b136b8851f176", null ],
-    [ "sir_plugin_init", "group__plugin__exports.html#ga16ed2c82f4bf0b4571c2d79bb946ce2d", null ],
-    [ "sir_plugin_write", "group__plugin__exports.html#ga8bcc79c4667749f5c81e6b1f22db0f1d", null ],
-    [ "sir_plugin_cleanup", "group__plugin__exports.html#ga43748071f13fd42b462fab8e3ea9faef", null ]
-];
--- docs/namespacesir_structsir_1_1error_dup.js
+++
@@ -1,5 +0,0 @@
-var namespacesir_structsir_1_1error_dup =
-[
-    [ "code", "namespacesir.html#a4859a24ff453b0a05a7824a824e7117a", null ],
-    [ "message", "namespacesir.html#a9f669faed973601b349f315ca7b3c053", null ]
-];
