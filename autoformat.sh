adac1b36760fcf377b9be5b37573c627c9d25229
Jeffrey H. Johnson <trnsz@pobox.com>
2023-12-26 05:06:47 -0500

    Cppi: Avoid mcmb and .git/* paths
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- autoformat.sh
+++ autoformat.sh
@@ -61,8 +61,9 @@ run_cppi()
   printf '%s' "Formatting with cppi ..."
   ( # shellcheck disable=SC2038
     find . -name "*.[ch]" -o -name "*.cc" -o -name "*.hh" | \
-      xargs -I{} "${SHELL:-sh}" -c \
-        'set -e; cppi "{}" > "{}.cppi" && mv -f "{}.cppi" "{}"'
+      grep -Ev '(mcmb\.c|\.git/.*)' | \
+        xargs -I{} "${SHELL:-sh}" -c \
+          'set -e; cppi "{}" > "{}.cppi" && mv -f "{}.cppi" "{}"'
   ) && printf '%s\n' "complete."
 }
 
