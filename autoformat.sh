8cd788b96c3a92de7ac6d084790c09de5eace55a
Jeffrey H. Johnson <trnsz@pobox.com>
2024-01-07 13:13:43 +0000

    Exclude build (#378)
    * Exclude build
    
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- autoformat.sh
+++ autoformat.sh
@@ -61,7 +61,7 @@ run_cppi()
   printf '%s' "Formatting with cppi ... "
   ( # shellcheck disable=SC2038
     find . -name "*.[ch]" -o -name "*.cc" -o -name "*.hh" | \
-      grep -Ev '(mcmb\.c|\.git/.*)' | \
+      grep -Ev '(build/.*|mcmb\.c$|\.git/.*)' | \
         xargs -I{} "${SHELL:-sh}" -c \
           'set -e; cppi "{}" > "{}.cppi" && mv -f "{}.cppi" "{}"'
   ) && printf '%s\n' "complete."
