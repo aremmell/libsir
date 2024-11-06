578ce6f6a35e7ab1bbcfac2b65ad840722db2b2f
Jeffrey H. Johnson <trnsz@pobox.com>
2024-11-06 01:49:05 +0000

    CI Updates and rebuild documentation (#440)
    * CI Updates
    * Rebuild documentation
    * Temporarily disable `file-archive-large` test for Emscripten builds
    
    ---------
    
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- docs/search/typedefs_1.js
+++ docs/search/typedefs_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['default_5flogger_0',['default_logger',['../namespacesir.html#ac3c8132d7d64f839a8eebb8f91c3d43c',1,'sir']]]
+  ['default_5flogger_0',['default_logger',['../namespacesir.html#afea168ed2dc59a4b8ab004364a6f4112',1,'sir']]]
 ];
