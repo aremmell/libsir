bfb32a993bb54b77dbd3d5d16d8404cbc47a5aad
RML <lederman@gmail.com>
2026-04-09 05:09:24 -0600

    Update.... docs? (#461)
    - Upgraded Doxygen config file;
    - Upgraded Layout;
    - Built with graphviz/htags installed.
    
    ---------
    
    Signed-off-by: Ryan M. Lederman <lederman@gmail.com>
    Signed-off-by: RML <lederman@gmail.com>

--- docs/search/defines_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['emph_0',['EMPH',['../ansimacros_8h.html#a83167d57009cd2bb4a9116de97039bf3',1,'ansimacros.h']]]
-];
--- docs/search/defines_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['green_0',['GREEN',['../ansimacros_8h.html#ac053f50f4f3531b32713826a0a5f3caa',1,'ansimacros.h']]],
-  ['greenb_1',['GREENB',['../ansimacros_8h.html#ad8ca0001ade83c6aaa041898f5dea562',1,'ansimacros.h']]]
-];
