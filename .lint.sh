4769849054a9a5a8aedc93014dce5f8dbe81b11b
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-05-09 22:56:27 -0600

    Appease new G++ 16 warning restrictions
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -377,7 +377,6 @@ test_gccextra()
         env CC="${CCACHE:-env} gcc"
             CXX="${CCACHE:-env} g++"
             CFLAGS="-Werror
-                    -Wbad-function-cast
                     -Wconversion
                     -Wdouble-promotion
                     -Wformat=2
@@ -385,7 +384,6 @@ test_gccextra()
                     -Wformat-overflow
                     -Wformat-truncation=2
                     -Wmissing-declarations
-                    -Wmissing-prototypes
                     -Wno-sign-conversion
                     -Wno-string-conversion
                     -Wswitch-enum
